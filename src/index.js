679a94b14a5fa1d52c55c3d03a920c095045f0e9
Johnkoderr <alexkool3008@gmail.com>
2025-08-15 22:59:37 +0800

    pre deploy

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ const content = document.getElementById('content');
 const homeBtn = document.getElementById('home-btn');
 const menuBtn = document.getElementById('menu-btn');
 const contactBtn = document.getElementById('contact-btn');
-
+// remove all children of content div and append section
 function replaceHTML(section) {
     content.replaceChildren();  
     content.append(section);
 }
-
+// Button event listeners
 homeBtn.addEventListener('click', ()=> replaceHTML(home()));
 menuBtn.addEventListener('click', ()=> replaceHTML(menu()));
 contactBtn.addEventListener('click', ()=> replaceHTML(contact()));
+// Initial Display
+content.append(home());
